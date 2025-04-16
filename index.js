d883af11b3ac5fe419051276e513dbf3b6e4e8c9
Kiany <sandovalkiany@gmail.com>
2025-04-15 23:15:02 -0600

    Cambios en el index.js para usar GraphQL

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const startApolloServer = require("../graphql"); 
+
 
 const app = express();
 
@@ -43,6 +45,12 @@ app.use("/perfiles", perfilesRoutes);
 app.use("/playlist", playlistRoutes);
 app.use("/videos", videosRoutes);
 
-// Iniciar servidor
+// Iniciar servidor HTTP y GraphQL
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+  console.log(`GraphQL disponible en http://localhost:${PORT}/graphql`);
+});
+
+// Inicializar Apollo Server 
+startApolloServer(app);
